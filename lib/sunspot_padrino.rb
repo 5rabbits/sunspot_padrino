a61f4ab4813b0f6dbb09e57f6125e3dd8d4fe006
Jordan Yaker <jordan.e.yaker@gmail.com>
2015-05-07 22:22:42 -0400

    Removed the attempt to add generators by the gem.

--- lib/sunspot_padrino.rb
+++ lib/sunspot_padrino.rb
@@ -7,10 +7,7 @@ end
 require 'sunspot/padrino'
 require 'sunspot/padrino/init'
 
-# Add our generators to Padrino::Generators.
-Padrino::Generators.load_paths << Dir[File.dirname(__FILE__) + '/../generators/*.rb']
-
 if defined? Padrino::Tasks
-# Add our tasks to padrino-core.
+  # Add our tasks to padrino-core.
   Padrino::Tasks.files << File.dirname(__FILE__) + '/sunspot/padrino/cli/tasks.rb'
 end
